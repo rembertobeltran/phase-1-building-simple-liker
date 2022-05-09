9afceab8fb61d3c039dec11d639507eae967dd45
Remberto <rembertobeltran@icloud.com>
2022-05-08 20:30:45 -0400

    Completed assignment

--- main.js
+++ main.js
@@ -4,7 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let serverError = document.querySelector("#modal-message");
+let heartGlyph = document.querySelectorAll(".like-glyph");
 
+let hidden = document.querySelector("#modal");
+hidden.classList.add("hidden");
+
+heartGlyph.forEach((e) => {
+  e.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        if (e.textContent !== '♥') {
+          e.classList.add("activated-heart");
+          e.textContent = '♥';
+        }
+        else if (e.textContent === '♥') {
+          e.textContent = EMPTY_HEART;
+          e.classList.remove('♡')
+        }
+      })
+      .catch((error) => {
+        hidden.classList.remove("hidden");
+        serverError.textContent = error;
+        setTimeout(hide, 3000);
+      })
+  })
+})
+
+function hide() {
+  return hidden.classList.add("hidden");
+}
 
 
 //------------------------------------------------------------------------------
